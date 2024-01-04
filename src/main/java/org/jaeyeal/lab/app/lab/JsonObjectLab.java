87f0d22b76c0e2ece7afd1da55c013c5e37ce5ba
jyyoun <jyyoun@aistudio.co.kr>
2024-01-04 10:28:10 +0900

    Gson 라이브러리 JsonObject()

--- src/main/java/org/jaeyeal/lab/app/lab/JsonObjectLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/JsonObjectLab.java
@@ -1,18 +1,30 @@
 package org.jaeyeal.lab.app.lab;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 
 public class JsonObjectLab {
     public static void main(String[] args) {
-        JsonObject obj = new JsonObject();
-        JsonObject subObj = new JsonObject();
-        subObj.addProperty("M","img1");
-        subObj.addProperty("1","img2");
-        subObj.addProperty("2","img3");
-        subObj.addProperty("3","img4");
-        obj.add("position", subObj);
-        System.out.println("obj = " + obj);
+        Gson gson = new Gson();
+        JsonObject syncMessageObject = new JsonObject();
+
+        JsonObject positionObject = new JsonObject();
+        positionObject.addProperty("M", "main1");
+        positionObject.addProperty("1", "path1");
+        positionObject.addProperty("2", "path2");
+        positionObject.addProperty("3", "path3");
+        positionObject.addProperty("4", "path4");
+        positionObject.addProperty("5", "path5");
+
+        syncMessageObject.add("position", positionObject);
+
+        syncMessageObject.addProperty("workSetNamePrefix", "SW");
+
+        System.out.println("syncMessageObject = " + syncMessageObject);
+        String syncMessageObjectStr = gson.toJson(syncMessageObject);
+        System.out.println("syncMessageObjectStr = " + syncMessageObjectStr);
+
 
     }
 }
