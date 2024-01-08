b47499448a89c41d921d094e60ac9659c4540fab
jyyoun <jyyoun@aistudio.co.kr>
2024-01-08 16:51:19 +0900

    volatile 사용

---
+++ src/main/java/org/jaeyeal/lab/app/lab/VolatileThreadNotSafeLab.java
@@ -0,0 +1,18 @@
+package org.jaeyeal.lab.app.lab;
+
+public class VolatileThreadNotSafeLab {
+    private static volatile long count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int maxCnt = 1000;
+
+        for(int i = 0; i < maxCnt; i++) {
+            new Thread(() -> {
+                count++;
+                System.out.println(count);
+            }).start();
+        }
+
+        Thread.sleep(100);
+    }
+}
