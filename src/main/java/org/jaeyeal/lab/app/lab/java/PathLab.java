905594e5d9c04d9a38e565e691e6732b6eb0d2a3
jyyoun <jyyoun@aistudio.co.kr>
2024-01-08 09:54:34 +0900

    Files.walk() 예시

--- src/main/java/org/jaeyeal/lab/app/lab/java/PathLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/java/PathLab.java
@@ -1,13 +1,20 @@
 package org.jaeyeal.lab.app.lab.java;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathLab {
-    public static void main(String[] args) {
-        String basePathStr = "\\\\192.168.1.224\\Zone_hyoworld\\jy\\multi-sample-8\\smp1\\img1";
+    public static void main(String[] args) throws IOException {
+        String basePathStr = "\\\\192.168.1.224\\aistudionas\\aistudio\\kor\\hyoworld\\202401\\image";
         Path basePath = Paths.get(basePathStr);
+        List<Path> collect = Files.walk(basePath).collect(Collectors.toList());
+        collect.forEach(i -> System.out.println(i));
+        System.out.println("=====");
         System.out.println("basePath = " + basePath);
         System.out.println("basePath.getRoot() = " + basePath.getRoot());
         // getParent() : 현재 경로의 부모를 반환
