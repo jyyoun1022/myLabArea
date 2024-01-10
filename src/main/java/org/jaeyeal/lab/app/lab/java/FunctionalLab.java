487432c6d2639748df605032665c3a856fc26a8b
jyyoun <jyyoun@aistudio.co.kr>
2024-01-10 12:50:26 +0900

    [Feature] java
     Functional_Function

---
+++ src/main/java/org/jaeyeal/lab/app/lab/java/FunctionalLab.java
@@ -0,0 +1,27 @@
+package org.jaeyeal.lab.app.lab.java;
+
+public class FunctionalLab {
+
+    // Function
+    @FunctionalInterface
+    public interface Function<T, R> {
+        R apply(T t);
+    }
+    /**
+     * Function 인터페이스는 apply 메서드 하나를 추상 메서드로 제공한다.
+     */
+    // 1. 구현 클래스 사용
+    static class Adder implements Function<Integer, Integer> {
+        @Override
+        public Integer apply(Integer x) {
+            return x * 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> myAdder = new Adder();
+
+        int result = myAdder.apply(5);
+        System.out.println("result = " + result);
+    }
+}
