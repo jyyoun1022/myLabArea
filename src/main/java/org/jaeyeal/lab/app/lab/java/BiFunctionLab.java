0614fe2a2c2defb51c973626485de57a6b9b280f
jyyoun <jyyoun@aistudio.co.kr>
2024-01-10 13:01:50 +0900

    [Feature] java
     Functional_TriFunction_maskCustom

--- src/main/java/org/jaeyeal/lab/app/lab/java/BiFunctionLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/java/BiFunctionLab.java
@@ -7,11 +7,21 @@ public class BiFunctionLab {
     @FunctionalInterface
     public interface BiFunction<T, U, R> {
         R apply(T t, U u);
-    }
+   }
+
+   @FunctionalInterface
+   public interface TriFunction<T, U, V, R> {
+        R apply(T t, U u ,V v);
+   }
+   // 인자를 3개 받는 함수형 인터페이스 Custom
 
     public static void main(String[] args) {
         BiFunction<Integer, Integer, Integer> add = (x,y) -> x + y;
         Integer result = add.apply(8, 5);
         System.out.println("result = " + result);
     }
+    /**
+     * @FunctionalInterface
+     * 는 단 하나의 abstract method(추상메서드)만을 갖는 인터페이스
+     */
 }
