486e652d94747510e2640643292ed87bfc517a5e
jyyoun <jyyoun@aistudio.co.kr>
2024-01-12 15:33:16 +0900

    [Feature] java
    Mean Stream Intermediate Operations, Terminal Operation

--- src/main/java/org/jaeyeal/lab/app/lab/java/StreamTerminalOperationLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/java/StreamTerminalOperationLab.java
@@ -4,7 +4,21 @@ public class StreamTerminalOperationLab {
     // 스트림의 종결 처리는 Stream 안의 데이터를 모아 반환하는 역할을 한다.
     // 중간처리 작업은 바로바로 실행이 되는 것이 아니라 종결 처리의 실행이
     // 필요할 때서야 비로소 중간 처리가 실행(Lazy Evaluation)
+
+    /**
+     *  Optional<T> max(Comparator<? super T> comparator);
+     *  - max
+     *      : Stream 안의 데이터 중 최대값을 반환한다.
+     *      : Stream이 비어 있다면 빈 Optional 을 반환한다.
+     *  Optional<T> min(Comparator<? super T> comparator);
+     *  - min
+     *      : Stream 안의 데이터 중 최소값을 반환
+     *      : Stream이 비어 있다면 빈 Optional 을 반환
+     *  long count();
+     *  - count : Stream 안의 데이터 개수를 반환
+     */
     public static void main(String[] args) {
+
 
     }
 }
