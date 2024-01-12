b28c7aa1d004b08f23637294205a0a2c0fa432c2
jyyoun <jyyoun@aistudio.co.kr>
2024-01-12 15:31:27 +0900

    [Feature] java
    Mean Stream Intermediate Operations, Terminal Operation

---
+++ src/main/java/org/jaeyeal/lab/app/lab/java/StreamTerminalOperationLab.java
@@ -0,0 +1,10 @@
+package org.jaeyeal.lab.app.lab.java;
+
+public class StreamTerminalOperationLab {
+    // 스트림의 종결 처리는 Stream 안의 데이터를 모아 반환하는 역할을 한다.
+    // 중간처리 작업은 바로바로 실행이 되는 것이 아니라 종결 처리의 실행이
+    // 필요할 때서야 비로소 중간 처리가 실행(Lazy Evaluation)
+    public static void main(String[] args) {
+
+    }
+}
