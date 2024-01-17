756330211e47a560a4d608c7879f4e2602b4663e
jyyoun <jyyoun@aistudio.co.kr>
2024-01-17 15:07:44 +0900

    [Feature] java
    ConcurrentHashMap

---
+++ src/main/java/org/jaeyeal/lab/app/lab/java/ConcurrentHashMapLab.java
@@ -0,0 +1,11 @@
+package org.jaeyeal.lab.app.lab.java;
+
+public class ConcurrentHashMapLab {
+    /**
+     *  ConcurrentHashMap 은 쓰기 작업에 동기화가 되어 있는 HashMap인데 거의 모든 면에서 HashMap 보다 성능이 좋다.
+     *  하지만 key,value 에 null을 허용하지 않는다.
+     */
+    public static void main(String[] args) {
+
+    }
+}
