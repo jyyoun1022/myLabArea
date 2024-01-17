f62bf543a8c7e2d93a8cb850707ec2f8d08c5ca5
jyyoun <jyyoun@aistudio.co.kr>
2024-01-17 14:57:32 +0900

    리스트 배열로 바꾸기

--- src/main/java/org/jaeyeal/lab/app/lab/java/AlgorithmLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/java/AlgorithmLab.java
@@ -20,6 +20,7 @@ public class AlgorithmLab {
 
         //  2. toArray()
         String[] result = list.toArray(new String[0]);
+        // 파라미터를 0을 주면 list 에 길이에 맞게 알아서 조정된다.
         System.out.println(Arrays.toString(result));
 
 
