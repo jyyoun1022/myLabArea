437104eccc1a1fcc64b567dbdc762e7d8c915190
jyyoun <jyyoun@aistudio.co.kr>
2024-01-19 15:02:18 +0900

    스프링

---
+++ src/main/java/org/jaeyeal/lab/app/lab/spring/SpringLab.java
@@ -0,0 +1,12 @@
+package org.jaeyeal.lab.app.lab.spring;
+
+public class SpringLab {
+    /** 스프링
+     * 자바 언어 기반의 프레임워크
+     * 좋은 객체 지향 프로그래밍을 잘할 수 있도록 도와주는 프레임워크
+     */
+
+    /** 좋은 객체 지향 프로그래밍
+     *
+     */
+}
