ec65e8562d2b34185eb4ce2e815e112ebf77ff5a
jyyoun <jyyoun@aistudio.co.kr>
2024-01-22 15:54:34 +0900

    [Feature] Spring
    싱글톤예시

--- src/main/java/org/jaeyeal/lab/app/lab/spring/SingleTonLab.java
+++ src/main/java/org/jaeyeal/lab/app/lab/spring/SingleTonLab.java
@@ -1,6 +1,59 @@
 package org.jaeyeal.lab.app.lab.spring;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+@Configuration
 public class SingleTonLab {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public OrderService orderService() {
+        return new OrderService(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+        System.out.println(memoryMemberRepository);
+        return memoryMemberRepository;
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new DiscountPolicy();
+    }
+
+    @RequiredArgsConstructor
+    public static class MemberServiceImpl implements MemberService {
+        private final MemberRepository memberRepository;
+    }
+
+    public interface MemberService {
+
+    }
+
+    @RequiredArgsConstructor
+    public static class OrderService {
+        private final MemberRepository memberRepository;
+        private final DiscountPolicy discountPolicy;
+    }
+
+    public static class DiscountPolicy {
+
+    }
+    public interface MemberRepository {
+
+    }
+
+    public static class MemoryMemberRepository implements MemberRepository {
+    }
+
     public static void main(String[] args) {
         /** 1. 싱글톤
          *
@@ -9,6 +62,21 @@ public class SingleTonLab {
          * 서로 다른 인스턴스를 가져오는게 아니라, 하나의 인스턴스를 참조하는것.
          */
 
+        /** 2. 싱글톤 주의점
+         *
+         *  싱글톤 패턴은 여러 클라이언트가 하나의 같은 객체를 공유하기 때문에 싱글톤 객체는 상태를 유지하게 설게하면 안된다.(stateless 하게 해야함)
+         *  - 특정 클라이언트에 의존적인 필드가 있으면 안된다.
+         *  - 값을 변경할 수 있는 필드가 있으면 안된다.
+         *  - 필드 대신 공유되지 않는 지역변수, 파라미터, ThreadLocal 등을 사용해야 한다.
+         *
+         *  예시) 멀티쓰레드
+         *  사용자 1이 싱글톤 객체 필드에 값을 1로 저장해두고, 10분 뒤에 값을 불러왔다고 하자
+         *  하지만 그 10분 사이에 사용자 2가 실글톤 객체의 필드값을 0으로 수정했다면,
+         *  사용자 1은 불러올 값을 1로 예상했지만 실제로는 0을 가져오게 된다.
+         *  싱글톤 패턴 즉, 객체를 공유하고 있기 때문에 이런 문제가 발생하는 것이다.
+         *  따라서 싱글톤 객체의 필드는 작성하지 않는게 좋다.
+         *
+         */
 
     }
 }
