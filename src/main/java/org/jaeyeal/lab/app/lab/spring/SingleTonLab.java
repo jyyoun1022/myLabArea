6f4f02e3a87b619017fbe26310e621eb912e81cf
jyyoun <jyyoun@aistudio.co.kr>
2024-01-22 15:30:27 +0900

    [Feature] Spring
    싱글톤이란

---
+++ src/main/java/org/jaeyeal/lab/app/lab/spring/SingleTonLab.java
@@ -0,0 +1,14 @@
+package org.jaeyeal.lab.app.lab.spring;
+
+public class SingleTonLab {
+    public static void main(String[] args) {
+        /** 1. 싱글톤
+         *
+         * 스프링 컨테이너는 알아서 코드를 짜주면서 자동으로 싱글톤 방식으로 데이터를 저장
+         * 따라서, 스프링 컨테이너에서 꺼내서 쓰는 데이터는 필요할 때마다 새로운 인스턴스를 만들어서
+         * 서로 다른 인스턴스를 가져오는게 아니라, 하나의 인스턴스를 참조하는것.
+         */
+
+
+    }
+}
